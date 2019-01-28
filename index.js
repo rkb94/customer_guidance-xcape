bc27dbfc4a4b7adf877899c19c05df1a132d8495
Kyungbin Ryu <rkbin@hayanmind.com>
2019-01-28 22:48:40 +0900

    Add main page

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as xcape} from './app.json';
+
+AppRegistry.registerComponent(xcape, () => App);
